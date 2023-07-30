3caf4bdef8823c787f7ba78f85890746d998a0a7
garrettladley <ladley.g@northeastern.edu>
2023-07-30 00:19:03 -0400

    initial

--- src/model/types.rs
+++ src/model/types.rs
@@ -1,4 +1,7 @@
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 use std::time::Duration;
+use strum::EnumIter;
 
 use serde::{Deserialize, Serialize};
 
@@ -8,4 +11,55 @@ pub struct Applicant {
     pub ok: bool,
     pub name: String,
     pub nuid: String,
+}
+
+#[derive(EnumIter, Debug)]
+pub enum Color {
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Violet,
+}
+
+#[derive(thiserror::Error, Serialize, Deserialize, Debug)]
+pub enum ColorParseError {
+    #[error("Invalid color")]
+    InvalidColor { given_color: String },
+}
+
+impl FromStr for Color {
+    type Err = ColorParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "red" => Ok(Color::Red),
+            "orange" => Ok(Color::Orange),
+            "yellow" => Ok(Color::Yellow),
+            "green" => Ok(Color::Green),
+            "blue" => Ok(Color::Blue),
+            "violet" => Ok(Color::Violet),
+            _ => Err(ColorParseError::InvalidColor {
+                given_color: s.to_string(),
+            }),
+        }
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Color::Red => "red",
+                Color::Orange => "orange",
+                Color::Yellow => "yellow",
+                Color::Green => "green",
+                Color::Blue => "blue",
+                Color::Violet => "violet",
+            }
+        )
+    }
 }
